3b0da5e55a0507ea37f00426068d0de4a570a59f
Laura-VFA <laura.villa@uclm.es>
2024-12-18 17:54:14 +0100

    replace old camera (realsense) library by picamera2

--- services/camera.py
+++ services/camera.py
@@ -3,35 +3,37 @@ from threading import Lock
 
 import imutils
 import numpy as np
-import pyrealsense2.pyrealsense2 as rs
-
+from picamera2 import Picamera2
 
 class Camera:
-    def __init__(self) -> None:
+    def __init__(self, resolution=(1280,720)) -> None:
         self.logger = logging.getLogger('Camera')
         self.logger.setLevel(logging.DEBUG)
 
         self.active_services = set() # set of services using the camera
 
-        self.pipeline = rs.pipeline()
-        self.config = rs.config()
-        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        self.camera = Picamera2()
+        # Configure resolution and format
+        self.camera_config = self.camera.create_preview_configuration(
+            main={"size": resolution, "format": "RGB888"}
+        )
+        self.camera.configure(self.camera_config)
 
         self.lock = Lock()
 
         self.logger.info('Ready')
 
     def get_color_frame(self, resize_width: int = None):
-        frames = self.pipeline.wait_for_frames()
-        color_image = np.asanyarray(frames.get_color_frame().get_data())
+        with self.lock:
+            frame = self.camera.capture_array().astype(np.uint8)
         if resize_width:
-            return imutils.resize(color_image, width=resize_width)
-        return color_image
+            return imutils.resize(frame, width=resize_width)
+        return frame
     
     def start(self, service):
         with self.lock: # exclusive access to set of services
             if not self.active_services:
-                self.pipeline.start(self.config)
+                self.camera.start()
             
             self.active_services.add(service)
         
@@ -45,7 +47,6 @@ class Camera:
             self.active_services.discard(service)
 
             if not self.active_services:
-                self.pipeline.poll_for_frames() # clear last frame buffer
-                self.pipeline.stop()
+                self.camera.stop()
         
         self.logger.info(f'Service {service} disabled')
