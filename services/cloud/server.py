23f5a2dd0223b5dcd0d8b694bfd6a06b4466f065
Laura-VFA <laura.villa@uclm.es>
2025-09-22 11:25:35 +0200

    added second timer count for APIs (generation time in log)

--- services/cloud/server.py
+++ services/cloud/server.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from dataclasses import dataclass
 
 from .google_api import speech_to_text, text_to_speech
@@ -28,12 +29,12 @@ class Response:
 
 def query(request: Request):
     # STT
+    start_time = time.time()
     request.text = speech_to_text(request.audio)
-    logger.info(f"STT result :: '{request.text}'")
+    logger.info(f"STT result ({time.time() - start_time:.2f} seconds) :: '{request.text}'")
     
     if not request.text:
         return None
-
 
     # Set context variables
     context_variables = {}
@@ -42,13 +43,16 @@ def query(request: Request):
     logger.info(f'Query context :: {context_variables}')
 
     # Generate the response
+    start_time = time.time()
     text_response, robot_context = generate_response(request.text, context_variables)
+    logger.info(f'LLM response generated in {time.time() - start_time:.2f} seconds')
     logger.info(f'Response text :: {text_response}')
     logger.info(f'Response context :: {robot_context}')
 
     # TTS
+    start_time = time.time()
     audio_response = text_to_speech(text_response)
-    logger.info("TTS result obtained")
+    logger.info(f"TTS result obtained (response generated in {time.time() - start_time:.2f} seconds)")
 
     # Send back the response
     return Response(
@@ -70,13 +74,16 @@ def proactive_query(request: Request):
     logger.info(f'Query context :: {context_variables}')
 
     # Generate the response
+    start_time = time.time()
     text_response, robot_context = generate_response('', context_variables) # Empty input_text since it's a proactive question
+    logger.info(f'LLM response generated in {time.time() - start_time:.2f} seconds')
     logger.info(f'Response text :: {text_response}')
     logger.info(f'Response context :: {robot_context}')
 
     # TTS
+    start_time = time.time()
     audio_response = text_to_speech(text_response)
-    logger.info("TTS result obtained")
+    logger.info(f"TTS result obtained (response generated in {time.time() - start_time:.2f} seconds)")
 
     # Send back the response
     return Response(
